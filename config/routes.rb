8b37c0c6da1bfe3a1cdf194dc37ce39e7a186392
EC2 Default User <ec2-user@ip-172-31-27-180.ap-northeast-1.compute.internal>
2023-10-04 08:42:14 +0000

    before submit 2

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,13 @@ Rails.application.routes.draw do
   resources :books, only: [:new, :index, :show, :create, :edit, :destroy, :update] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
+  end
+  
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+      	get "followings" => "relationships#followings", as: "followings"
+      	get "followers" => "relationships#followers", as: "followers"
   end 
-  resources :users, only: [:show, :edit, :index, :update]
   root to: "homes#top"
   get '/home/about' => "homes#about", as: "homeabout"
 end
